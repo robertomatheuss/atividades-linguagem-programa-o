ebb2f81219045cf86ec4566f9a9abfe0bdafa473
Roberto Matheus <robertmatheusa@gmail.com>
2024-07-22 14:45:11 -0300

    Alteração no nome

---
+++ 1 - atividade/atividade_pesquisa_album/domain.py
@@ -0,0 +1,15 @@
+from tkinter import * 
+
+from gui import *
+import db
+
+def adicionaAlbum(nomeAlbum,nomeBanda,autorLançamento,anoLancamento):
+    db.escreveArquivo(nomeAlbum,nomeBanda,autorLançamento,anoLancamento)
+    
+def coletaDados():
+    linha = db.lerArquivo()
+    del linha[-1]
+    listaComElementos= []
+    for elemento in linha:
+        listaComElementos.append(elemento.split("|"))
+    return listaComElementos
